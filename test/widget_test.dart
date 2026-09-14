c5eda3b42e5c435ba0dfcda4ad17037e04f8437c
Liane Heidemann <haruru.myra@gmail.com>
2026-09-14 14:30:16 -0300

    Release v1.1.0: align interface widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@ void main() {
   testWidgets('configures and opens a workout', (tester) async {
     await tester.pumpWidget(const SquatCounterApp());
 
-    expect(find.text('Configure your workout'), findsOneWidget);
+    expect(find.text('Configure your\nworkout'), findsOneWidget);
     expect(find.text('30 squats'), findsOneWidget);
 
     await tester.tap(find.widgetWithIcon(IconButton, Icons.add_rounded).first);
@@ -16,7 +16,7 @@ void main() {
     await tester.tap(find.text('Start workout'));
     await tester.pumpAndSettle();
 
-    expect(find.text('Workout in progress'), findsOneWidget);
+    expect(find.text('Squat Counter'), findsOneWidget);
     expect(find.text('Start counting'), findsOneWidget);
   });
 }
